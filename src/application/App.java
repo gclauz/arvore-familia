a3280d7ae91594ebbbcd7a668f733c8dbba26293
Guilherme Clauz <guiclauz13@gmail.com>
2025-09-07 14:03:49 -0300

    teste-leitura

---
+++ src/application/App.java
@@ -0,0 +1,10 @@
+package application;
+
+import util.FileUtils;
+
+public class App {
+    public static void main(String[] args) {
+        FileUtils fileUtils = new FileUtils();
+        fileUtils.readFile();
+    }
+}
