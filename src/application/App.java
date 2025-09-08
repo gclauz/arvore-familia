8e06f0f809550eb0c2c85239cd5cd2455f817a10
Gabriel <gabriel.hideaquy@gmail.com>
2025-09-08 19:14:01 -0300

    while

--- src/application/App.java
+++ src/application/App.java
@@ -1,10 +1,20 @@
 package application;
 
 import util.FileUtils;
+import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
         FileUtils fileUtils = new FileUtils();
         fileUtils.readFile();
+
+        Scanner sc = new Scanner(System.in);
+        String input = "";
+
+        while (!input.equals("exit")) {
+            input = sc.next();
+        }
+
+
     }
 }
